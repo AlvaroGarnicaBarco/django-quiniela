8f877e1f095861d3f3289bae261a508ffa5bbefc
Alvaro Garnica Barco <Alvaro@MacBook-Pro-de-Alvaro.local>
2021-09-29 05:20:50 +0200

    Initial commit

---
+++ carga_Jugadas.py
@@ -0,0 +1,39 @@
+import os
+import django
+from apps.quiniela_app.models import Jugada
+import pandas as pd
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quiniela_old.settings')
+django.setup()
+
+df = pd.read_pickle("apps/quiniela_app/df.pkl")
+
+lista_jugadas = []
+for index in range(len(df)):
+    j = Jugada(
+        combinacion=df['Jugada'].iloc[index].replace(' ', ''),
+        pos_est=df['Posición estimada'].iloc[index],
+        pos_real=df['Posición real'].iloc[index],
+        unos=df['1s'].iloc[index],
+        equis=df['Xs'].iloc[index],
+        doses=df['2s'].iloc[index],
+        mas_probs=df['signos + probables'].iloc[index],
+        menos_probs=df['signos - probables'].iloc[index],
+        partido_1=df['partido_1'].iloc[index],
+        partido_2=df['partido_2'].iloc[index],
+        partido_3=df['partido_3'].iloc[index],
+        partido_4=df['partido_4'].iloc[index],
+        partido_5=df['partido_5'].iloc[index],
+        partido_6=df['partido_6'].iloc[index],
+        partido_7=df['partido_7'].iloc[index],
+        partido_8=df['partido_8'].iloc[index],
+        partido_9=df['partido_9'].iloc[index],
+        partido_10=df['partido_10'].iloc[index],
+        partido_11=df['partido_11'].iloc[index],
+        partido_12=df['partido_12'].iloc[index],
+        partido_13=df['partido_13'].iloc[index],
+        partido_14=df['partido_14'].iloc[index]
+    )
+    lista_jugadas.append(j)
+
+Jugada.objects.bulk_create(lista_jugadas)
---
+++ carga_Partidos.py
@@ -0,0 +1,147 @@
+import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quiniela_old.settings')
+django.setup()
+
+from apps.quiniela_app.models import Partido, PartidoPleno15
+import requests
+from bs4 import BeautifulSoup
+from apps.quiniela_app.config import jornada_actual, temporada_actual
+
+
+def carga_partidos():
+    respuesta = requests.get('https://www.eduardolosilla.es/')
+    soup = BeautifulSoup(respuesta.content, 'html.parser')
+
+    if soup.find('h3',
+                 class_='c-boleto-multiples-caja-base-header__container__jornada ng-star-inserted').text.strip().split()[
+        -1] != jornada_actual:
+        print('Jornada que se está intentando generar partidos no corresponde a la jornada actual')
+
+    else:
+        base = soup.find(id="body")
+        # carga partidos no pleno 15
+        partidos = base.find_all('div', class_='c-caja_base__partido u-clearfix ng-star-inserted')
+
+        for idx, partido in enumerate(partidos):
+            all_probs = partido.find_all(class_='u-txt-general c-boleto-multiples-porcentajes__row__normal')
+
+            orden_partido = idx + 1
+            dia = partido.find(class_='c-marcador-horario__time__day').text.strip()
+            hora = partido.find(class_='c-marcador-horario__time__hour').text.strip()
+            local = partido.find(class_='c-equipos__teams m-short ng-star-inserted').text.strip().split(' - ')[0]
+            visitante = partido.find(class_='c-equipos__teams m-short ng-star-inserted').text.strip().split(' - ')[1]
+            prob_real_1 = int(all_probs[6].text.strip())
+            prob_real_X = int(all_probs[7].text.strip())
+            prob_real_2 = int(all_probs[8].text.strip())
+            prob_est_1 = round(int(all_probs[0].text.strip()) * 0.2 + int(all_probs[3].text.strip()) * 0.8)
+            prob_est_X = round(int(all_probs[1].text.strip()) * 0.2 + int(all_probs[4].text.strip()) * 0.8)
+            prob_est_2 = round(int(all_probs[2].text.strip()) * 0.2 + int(all_probs[5].text.strip()) * 0.8)
+            rentabilidad_1 = round(prob_real_1 / prob_est_1, 2)
+            rentabilidad_X = round(prob_real_X / prob_est_X, 2)
+            rentabilidad_2 = round(prob_real_2 / prob_est_2, 2)
+
+            Partido.objects.update_or_create(
+                temporada=temporada_actual,
+                jornada=jornada_actual,
+                orden_partido=orden_partido,
+                defaults={
+                    'dia': dia,
+                    'hora': hora,
+                    'local': local,
+                    'visitante': visitante,
+                    'prob_real_1': prob_real_1,
+                    'prob_real_X': prob_real_X,
+                    'prob_real_2': prob_real_2,
+                    'prob_est_1': prob_est_1,
+                    'prob_est_X': prob_est_X,
+                    'prob_est_2': prob_est_2,
+                    'rentabilidad_1': rentabilidad_1,
+                    'rentabilidad_X': rentabilidad_X,
+                    'rentabilidad_2': rentabilidad_2
+                }
+            )
+
+        # carga partido pleno 15
+        partido_pleno = base.find(class_='c-caja_base__partido u-clearfix m-pleno15 ng-star-inserted')
+        all_probs_pleno = partido_pleno.find_all(class_='u-txt-general c-boleto-multiples-porcentajes__row__pleno')
+
+        dia15 = partido_pleno.find(class_='c-marcador-horario__time__day').text.strip()
+        hora15 = partido_pleno.find(class_='c-marcador-horario__time__hour').text.strip()
+        local15 = partido_pleno.find(class_='c-equipos__teams ng-star-inserted').text.strip().split('\n')[0]
+        visitante15 = partido_pleno.find(class_='c-equipos__teams ng-star-inserted').text.strip().split('\n')[1].strip()
+        prob_real_local_0 = int(all_probs_pleno[16].text.strip())
+        prob_real_local_1 = int(all_probs_pleno[17].text.strip())
+        prob_real_local_2 = int(all_probs_pleno[18].text.strip())
+        prob_real_local_M = int(all_probs_pleno[19].text.strip())
+        prob_real_visitante_0 = int(all_probs_pleno[20].text.strip())
+        prob_real_visitante_1 = int(all_probs_pleno[21].text.strip())
+        prob_real_visitante_2 = int(all_probs_pleno[22].text.strip())
+        prob_real_visitante_M = int(all_probs_pleno[23].text.strip())
+        prob_est_local_0 = round(
+            int(all_probs_pleno[0].text.strip()) * 0.2 + int(all_probs_pleno[8].text.strip()) * 0.8)
+        prob_est_local_1 = round(
+            int(all_probs_pleno[1].text.strip()) * 0.2 + int(all_probs_pleno[9].text.strip()) * 0.8)
+        prob_est_local_2 = round(
+            int(all_probs_pleno[2].text.strip()) * 0.2 + int(all_probs_pleno[10].text.strip()) * 0.8)
+        prob_est_local_M = round(
+            int(all_probs_pleno[3].text.strip()) * 0.2 + int(all_probs_pleno[11].text.strip()) * 0.8)
+        prob_est_visitante_0 = round(
+            int(all_probs_pleno[4].text.strip()) * 0.2 + int(all_probs_pleno[12].text.strip()) * 0.8)
+        prob_est_visitante_1 = round(
+            int(all_probs_pleno[5].text.strip()) * 0.2 + int(all_probs_pleno[13].text.strip()) * 0.8)
+        prob_est_visitante_2 = round(
+            int(all_probs_pleno[6].text.strip()) * 0.2 + int(all_probs_pleno[14].text.strip()) * 0.8)
+        prob_est_visitante_M = round(
+            int(all_probs_pleno[7].text.strip()) * 0.2 + int(all_probs_pleno[15].text.strip()) * 0.8)
+        rentabilidad_local_0 = round(prob_real_local_0 / prob_est_local_0, 2)
+        rentabilidad_local_1 = round(prob_real_local_1 / prob_est_local_1, 2)
+        rentabilidad_local_2 = round(prob_real_local_2 / prob_est_local_2, 2)
+        rentabilidad_local_M = round(prob_real_local_M / prob_est_local_M, 2)
+        rentabilidad_visitante_0 = round(prob_real_visitante_0 / prob_est_visitante_0, 2)
+        rentabilidad_visitante_1 = round(prob_real_visitante_1 / prob_est_visitante_1, 2)
+        rentabilidad_visitante_2 = round(prob_real_visitante_2 / prob_est_visitante_2, 2)
+        rentabilidad_visitante_M = round(prob_real_visitante_M / prob_est_visitante_M, 2)
+
+        PartidoPleno15.objects.update_or_create(
+            temporada=temporada_actual,
+            jornada=jornada_actual,
+            defaults={
+                'dia': dia15,
+                'hora': hora15,
+                'local': local15,
+                'visitante': visitante15,
+                'prob_real_local_0': prob_real_local_0,
+                'prob_real_local_1': prob_real_local_1,
+                'prob_real_local_2': prob_real_local_2,
+                'prob_real_local_M': prob_real_local_M,
+                'prob_real_visitante_0': prob_real_visitante_0,
+                'prob_real_visitante_1': prob_real_visitante_1,
+                'prob_real_visitante_2': prob_real_visitante_2,
+                'prob_real_visitante_M': prob_real_visitante_M,
+                'prob_est_local_0': prob_est_local_0,
+                'prob_est_local_1': prob_est_local_1,
+                'prob_est_local_2': prob_est_local_2,
+                'prob_est_local_M': prob_est_local_M,
+                'prob_est_visitante_0': prob_est_visitante_0,
+                'prob_est_visitante_1': prob_est_visitante_1,
+                'prob_est_visitante_2': prob_est_visitante_2,
+                'prob_est_visitante_M': prob_est_visitante_M,
+                'rentabilidad_local_0': rentabilidad_local_0,
+                'rentabilidad_local_1': rentabilidad_local_1,
+                'rentabilidad_local_2': rentabilidad_local_2,
+                'rentabilidad_local_M': rentabilidad_local_M,
+                'rentabilidad_visitante_0': rentabilidad_visitante_0,
+                'rentabilidad_visitante_1': rentabilidad_visitante_1,
+                'rentabilidad_visitante_2': rentabilidad_visitante_2,
+                'rentabilidad_visitante_M': rentabilidad_visitante_M
+            }
+        )
+
+
+if __name__ == "__main__":
+    print('Por favor espere...')
+    carga_partidos()
+    print('Se han cargado/actualizado los partidos de la jornada', jornada_actual,
+          'en las bases Partido y PartidoPleno15')
