70419182df3dfb5b3fbb784e9b9f55315d061de6
Alvaro <alvarogarnica1997@gmail.com>
2021-10-31 03:09:05 +0100

    implementación de auth login token

---
+++ apps/users/views.py
@@ -0,0 +1,7 @@
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.authtoken.views import ObtainAuthToken
+
+
+class Login(ObtainAuthToken):
+    pass
--- quiniela/urls.py
+++ quiniela/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, re_path, include
 
+from apps.users.views import Login
+
 from rest_framework import permissions
 
 # noinspection PyUnresolvedReferences
@@ -25,6 +27,7 @@ urlpatterns = [
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('admin/', admin.site.urls),
+    path('', Login.as_view(), name='login'),
     path('api/', include('apps.users.api.urls')),
     path('api/', include('apps.quiniela_main.api.urls')),
 ]
